c7d79c2b9a8087c27ab0377a5956f92f68463853
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2018-12-03 09:43:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const client = new Discord.Client();
 var prefix = "1";
 
-client.on('ready', () => {
+client.on('ready', (2) => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
@@ -18,10 +18,10 @@ client.on('message', message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
-  let command = message.content.split(" ")[0];
+  let command = message.content.split("spek")[0];
   command = command.slice(prefix.length);
 
-  let args = message.content.split(" ").slice(1);
+  let args = message.content.split("m").slice(1);
 
   if (command == "say") {
 if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
